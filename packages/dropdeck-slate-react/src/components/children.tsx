5e1754da78e70c2211a4bca9977a0bcc2137eb99
Bjarki Holm <bjarki.holm@gmail.com>
2023-12-19 12:37:42 +0000

    fix: Skip adding a child element to the Editable when an error occurs

--- packages/dropdeck-slate-react/src/components/children.tsx
+++ packages/dropdeck-slate-react/src/components/children.tsx
@@ -37,48 +37,52 @@ const Children = (props: {
     Editor.hasInlines(editor, node)
 
   for (let i = 0; i < node.children.length; i++) {
-    const p = path.concat(i)
-    const n = node.children[i] as Descendant
-    const key = ReactEditor.findKey(editor, n)
-    const range = Editor.range(editor, p)
-    const sel = selection && Range.intersection(range, selection)
-    const ds = decorate([n, p])
+    try {
+      const p = path.concat(i)
+      const n = node.children[i] as Descendant
+      const key = ReactEditor.findKey(editor, n)
+      const range = Editor.range(editor, p)
+      const sel = selection && range && Range.intersection(range, selection)
+      const ds = decorate([n, p])
 
-    for (const dec of decorations) {
-      const d = Range.intersection(dec, range)
+      for (const dec of decorations) {
+        const d = range && Range.intersection(dec, range)
 
-      if (d) {
-        ds.push(d)
+        if (d) {
+          ds.push(d)
+        }
       }
-    }
 
-    if (Element.isElement(n)) {
-      children.push(
-        <ElementComponent
-          decorate={decorate}
-          decorations={ds}
-          element={n}
-          key={key.id}
-          renderElement={renderElement}
-          renderLeaf={renderLeaf}
-          selection={sel}
-        />
-      )
-    } else {
-      children.push(
-        <TextComponent
-          decorations={ds}
-          key={key.id}
-          isLast={isLeafBlock && i === node.children.length - 1}
-          parent={node}
-          renderLeaf={renderLeaf}
-          text={n}
-        />
-      )
-    }
+      if (Element.isElement(n)) {
+        children.push(
+          <ElementComponent
+            decorate={decorate}
+            decorations={ds}
+            element={n}
+            key={key.id}
+            renderElement={renderElement}
+            renderLeaf={renderLeaf}
+            selection={sel}
+          />
+        )
+      } else {
+        children.push(
+          <TextComponent
+            decorations={ds}
+            key={key.id}
+            isLast={isLeafBlock && i === node.children.length - 1}
+            parent={node}
+            renderLeaf={renderLeaf}
+            text={n}
+          />
+        )
+      }
 
-    NODE_TO_INDEX.set(n, i)
-    NODE_TO_PARENT.set(n, node)
+      NODE_TO_INDEX.set(n, i)
+      NODE_TO_PARENT.set(n, node)
+    } catch (e) {
+      console.log(`Error when iterating over editor children`, e)
+    }
   }
 
   return <React.Fragment>{children}</React.Fragment>
